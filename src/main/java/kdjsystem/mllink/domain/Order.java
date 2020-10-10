baddf76fc94dfbeeee2f0f4f4fde959d3dd6edfc
김종민 <kjspp4@gmail.com>
2020-10-10 16:58:06 +0900

    연관관계 편의 메서드

--- src/main/java/kdjsystem/mllink/domain/Order.java
+++ src/main/java/kdjsystem/mllink/domain/Order.java
@@ -22,11 +22,11 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order",fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "order",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -34,4 +34,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    // == 연관관계 메서드 ==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrderList().add(this);
+    }
+
+    public  void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
